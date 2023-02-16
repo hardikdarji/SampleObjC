f2c9a4044ee9465e32bd713080f788b5da1b0c84
ilumi Solutions <hhardik.darji@ilumisolutions.com>
2023-02-16 17:46:15 +0530

    first commit

---
+++ sampleObjC/sampleObjC/sampleObjC.h
@@ -0,0 +1,18 @@
+//
+//  sampleObjC.h
+//  sampleObjC
+//
+//  Created by ilumi Solutions on 2/16/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sampleObjC.
+FOUNDATION_EXPORT double sampleObjCVersionNumber;
+
+//! Project version string for sampleObjC.
+FOUNDATION_EXPORT const unsigned char sampleObjCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sampleObjC/PublicHeader.h>
+
+
